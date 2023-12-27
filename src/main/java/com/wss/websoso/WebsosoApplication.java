715ca189c2567805d5df607261ae72c398e01eea
Kim-TaeUk <xodnr0130@naver.com>
2023-12-27 17:37:51 +0900

    [INIT] 프로젝트 세팅

---
+++ src/main/java/com/wss/websoso/WebsosoApplication.java
@@ -0,0 +1,13 @@
+package com.wss.websoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wss/websoso/WebsosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wss.websoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
