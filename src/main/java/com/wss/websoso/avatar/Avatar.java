8a99ef3e5a0ea3db9f6aef2bac0f68373d2f5a5b
Kim-TaeUk <xodnr0130@naver.com>
2024-01-11 08:21:01 +0900

    [FIX] Avatar 도메인 수정

--- src/main/java/com/wss/websoso/avatar/Avatar.java
+++ src/main/java/com/wss/websoso/avatar/Avatar.java
@@ -1,5 +1,6 @@
 package com.wss.websoso.avatar;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +15,30 @@ public class Avatar {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "avatar_id", nullable = false)
     private Long avatarId;
+
+    @Column(name = "avatar_tag", nullable = false)
     private String avatarTag;
-    private String avatarImg;
-    private String avatarComment;
-    private String avatarDescription;
-    private String avatarBadge;
+
+    @Column(name = "avatar_genre_badge_img", nullable = false)
+    private String avatarGenreBadgeImg;
+
+    @Column(name = "avatar_acquired_img", nullable = false)
+    private String avatarAcquiredImg;
+
+    @Column(name = "avatar_unacquired_img", nullable = false)
+    private String avatarUnacquiredImg;
+
+    @Column(name = "avatar_acquired_ment", nullable = false)
+    private String avatarAcquiredMent;
+
+    @Column(name = "avatar_unacquired_ment", nullable = false)
+    private String avatarUnacquiredMent;
+
+    @Column(name = "avatar_acquired_condition", nullable = false)
+    private String avatarAcquiredCondition;
+
+    @Column(name = "avatar_unacquired_condition", nullable = false)
+    private String avatarUnacquiredCondition;
 }
