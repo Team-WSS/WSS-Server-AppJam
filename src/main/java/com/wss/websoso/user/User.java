3a984a3efa1004049256ca9d2eefdf7448fa4805
Kim-TaeUk <xodnr0130@naver.com>
2024-01-11 08:21:45 +0900

    [FIX] User 도메인 수정

--- src/main/java/com/wss/websoso/user/User.java
+++ src/main/java/com/wss/websoso/user/User.java
@@ -1,5 +1,6 @@
 package com.wss.websoso.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,12 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
     private Long userId;
+
+    @Column(name = "user_nickname", nullable = false)
     private String userNickname;
+
+    @Column(name = "user_rep_avatar_id", nullable = false)
+    private Long userRepAvatarId;
 }
