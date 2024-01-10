814ed7293c4a0db9aacfe4a098dce3533c82842e
Kim-TaeUk <xodnr0130@naver.com>
2024-01-10 18:17:32 +0900

    [REFACTOR] endpoint의 HTTP 응답처리 개선
    ResponseEntity 사용

--- src/main/java/com/wss/websoso/userNovel/UserNovelController.java
+++ src/main/java/com/wss/websoso/userNovel/UserNovelController.java
@@ -4,6 +4,8 @@ import com.wss.websoso.config.ReadStatus;
 import java.security.Principal;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,9 +38,12 @@ public class UserNovelController {
     }
 
     @GetMapping("/{userNovelId}")
-    public UserNovelMemoAndInfoGetResponse getUserNovelMemoAndInfo(@PathVariable Long userNovelId,
-                                                                   Principal principal) {
+    public ResponseEntity<UserNovelMemoAndInfoGetResponse> getUserNovelMemoAndInfo(@PathVariable Long userNovelId,
+                                                                                   Principal principal) {
+
         Long userId = Long.valueOf(principal.getName());
-        return userNovelService.getUserNovelMemoAndInfo(userId, userNovelId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userNovelService.getUserNovelMemoAndInfo(userId, userNovelId));
     }
 }
