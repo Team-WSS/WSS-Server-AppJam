5fefe662ab40a45c07dc6ca27ef69bc46243cd04
ChaeAg <dydkglel123@gmail.com>
2024-01-14 01:43:24 +0900

    [FIX] 유저 대표 아바타 조회 API 수정
    명세의 변경으로 인해 반환값에서 아바타 이미지 제거

--- src/main/java/com/wss/websoso/avatar/UserRepAvatarGetResponse.java
+++ src/main/java/com/wss/websoso/avatar/UserRepAvatarGetResponse.java
@@ -3,14 +3,12 @@ package com.wss.websoso.avatar;
 public record UserRepAvatarGetResponse(
         String avatarTag,
         String avatarLine,
-        String avatarImg,
         String userNickname
 ) {
     public static UserRepAvatarGetResponse of(Avatar avatar, String avatarLine, String userNickname) {
         return new UserRepAvatarGetResponse(
                 avatar.getAvatarTag(),
                 avatarLine,
-                avatar.getAvatarAcquiredImg(),
                 userNickname
         );
     }
