084a32416b2a4ee385c06270c69a2446894c7980
Kim-TaeUk <xodnr0130@naver.com>
2024-01-14 08:20:49 +0900

    [REFACTOR] 누락된 @Repository 어노테이션 추가

--- src/main/java/com/wss/websoso/keyword/KeywordRepository.java
+++ src/main/java/com/wss/websoso/keyword/KeywordRepository.java
@@ -3,9 +3,7 @@ package com.wss.websoso.keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    Optional<Keyword> findByKeywordName(String keywordName);
+
 }
