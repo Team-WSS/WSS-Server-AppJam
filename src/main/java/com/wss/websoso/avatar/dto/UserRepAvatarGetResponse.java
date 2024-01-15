9c261ed297e6f95a73247a2f589caaff1218b19b
Kim-TaeUk <xodnr0130@naver.com>
2024-01-15 23:48:36 +0900

    [FIX] 대표 아바타 조회 API 반환값 추가
    대표 아바타 조회 API 반환값에 아바타 id 추가

--- src/main/java/com/wss/websoso/avatar/dto/UserRepAvatarGetResponse.java
+++ src/main/java/com/wss/websoso/avatar/dto/UserRepAvatarGetResponse.java
@@ -3,12 +3,14 @@ package com.wss.websoso.avatar.dto;
 import com.wss.websoso.avatar.Avatar;
 
 public record UserRepAvatarGetResponse(
+        Long avatarId,
         String avatarTag,
         String avatarLine,
         String userNickname
 ) {
     public static UserRepAvatarGetResponse of(Avatar avatar, String avatarLine, String userNickname) {
         return new UserRepAvatarGetResponse(
+                avatar.getAvatarId(),
                 avatar.getAvatarTag(),
                 avatarLine.replaceAll("%s", userNickname),
                 userNickname
